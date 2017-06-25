c0b0d888d80c28d253753af8908257673acc6f88
Han Se (Andrew) Lee <hs27lee@edu.uwaterloo.ca>
2017-06-25 18:25:09 -0400

    sqlite tables updated

---
+++ app/src/main/java/me/andrewhanselee/stayproductive/menu_page.java
@@ -0,0 +1,13 @@
+package me.andrewhanselee.stayproductive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_page);
+    }
+}
